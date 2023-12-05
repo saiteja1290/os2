5f1bbe369c4d84547fba61a5444869eb7a322710
saiteja.palegar@gmail.com <saiteja.palegar@gmail.com>
2023-12-05 22:52:39 +0530

    wefwef

--- paging.c
+++ paging.c
@@ -4,14 +4,17 @@ int main()
 {
     int page[MAX], i, n, f, ps, off, pno;
     int choice = 0;
-    printf("\nEnter the no of  peges in memory: ");
+    printf("\nEnter the no of pages in memory: ");
     scanf("%d", &n);
     printf("\nEnter page size: ");
     scanf("%d", &ps);
     printf("\nEnter no of frames: ");
     scanf("%d", &f);
     for (i = 0; i < n; i++)
+    {
         page[i] = -1;
+    }
+
     printf("\nEnter the page table\n");
     printf("(Enter frame no as -1 if that page is not present in any frame)\n\n");
     printf("\npageno\tframeno\n-------\t-------");
@@ -31,5 +34,6 @@ int main()
         printf("\nDo you want to continue(1/0)?:");
         scanf("%d", &choice);
     } while (choice == 1);
-    return 1;
+
+    return 0;
 }
